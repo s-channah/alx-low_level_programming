e9a4df7e35ba5c295b37b9216ca57665987856bf
s-channah <hannahsadofabohns@gmail.com>
2023-05-29 21:19:34 +0100

    Replace variable value with 48

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*  reset_to_98 - takes int pointer & updates variable value to 98
+*  @n: an integer pointer i.e. address of variable to be updated
+*  Return: nothing
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Pointing at Pointer
