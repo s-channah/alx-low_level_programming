b3d1f1d732557fd89073c84eb2aecc75a1ed0fe4
s-channah <hannahsadofabohns@gmail.com>
2023-06-05 22:56:15 +0100

    a function tha copies memory area of a ptr into memory area of another

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,14 +1,15 @@
 #include "main.h"
-/** _memcpy - copies @n 
- * 
- * 
- * 
- */
-
+/**
+* _memcpy - copies
+*  @dest: memory area that is the destination.
+*  @src: memory to source bytes
+*  @n:length of bytes to copy to dest
+*  Return: pointer to @dest
+*/
 char *_memcpy(char *dest, char *src, unsigned int n)
-(
-        unsigned int i;
-        for (i = 0; i < n; i++)
-                dest[i] = src[i];
-        return (dest);
-)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+dest[i] = src[i];
+return (dest);
+}
