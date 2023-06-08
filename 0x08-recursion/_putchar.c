9601e93c6a40cab1d6087dc07d029a0c53526725
s-channah <hannahsadofabohns@gmail.com>
2023-06-08 01:48:28 +0100

    completed task 0

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,7 @@
+#include "main.h"
+#include <stdio.h>
+
+int _putchar(char c)
+{
+        return (putchar(c));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,6 +8,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-int _putchar(char *);
+int _putchar(char s);
 
 #endif  /*Hannah Sado*/
