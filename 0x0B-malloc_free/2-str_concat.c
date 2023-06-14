7a66e34991c4e4d8284668354d876808d9336f02
s-channah <hannahsadofabohns@gmail.com>
2023-06-14 05:54:32 +0100

    updated task 2 on str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,6 +13,12 @@ char *str_concat(char *s1, char *s2)
 	int i, cat_i, len1, len2, len;
 	char *concated;
 
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
 	cat_i = len1 = len2 = 0;
 
 	for (i = 0; s1[i]; i++)
