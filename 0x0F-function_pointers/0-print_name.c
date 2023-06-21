a634884ebc32deb7a9f682ea8300a80c81adf161
s-channah <hannahsadofabohns@gmail.com>
2023-06-21 23:44:52 +0100

    simple use of function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints name
+ * @name: This is the name to be printed
+ * @f: pointer to functions that prints character arrays passed to it
+ * Return: void/ nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
