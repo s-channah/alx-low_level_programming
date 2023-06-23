0b8c82c8e09253a406206d958d174bf30425d4a4
s-channah <hannahsadofabohns@gmail.com>
2023-06-23 01:48:03 +0100

    creating  a variadic function that prints anything passed into it

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,48 @@
+#include "variadic_functions.h"
+/**
+ * print_all - print anything passed, with a \n
+ * @format: strings of characters repping argument types
+ * @...: variable argumemts to be printed
+ */
+void print_all(const char * const format, ...)
+{
+	unsigned int i = 0;
+	va_list args;
+	char *str;
+	int flag;
+
+	va_start(args, format);
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				flag = 0;
+				break;
+			case 'i':
+				printf("%i", va_arg(args, int));
+				flag = 0;
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				flag = 0;
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				flag = 0;
+				break;
+			default:
+				flag = 1;
+				break;
+		}
+		if (format[i + 1] != '\0' && flag == 0)
+			printf(",");
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
