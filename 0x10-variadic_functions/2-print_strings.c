ebb6ec7eff12a7b22c4677b6d090ae299dfef755
s-channah <hannahsadofabohns@gmail.com>
2023-06-23 01:12:46 +0100

    changed passing a direct string to passing string saved in variable now passed

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		str = va_arg(args, char *);
 		if (str == NULL)
-			printf("nil");
+			str = "nil";
+			printf("%s", str);
 		else
 			printf("%s", str);
 
