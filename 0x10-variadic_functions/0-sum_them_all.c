4293b005052e7121fc6a2a3fa90468f95207e219
s-channah <hannahsadofabohns@gmail.com>
2023-06-23 02:18:43 +0100

    changes

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,7 +9,8 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i, sum = 0;
+	int sum = 0;
+	unsigned int i;
 	va_list args;
 
 	if (n == 0)
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,29 +2,7 @@
 #define _VARIADIC_FUNCTIONS_H
 
 #include <stdio.h>
-
 #include <stdarg.h>
-
-/**
- * File: variadic_functions.h
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0F-variadic_functions directory.
- */
-
-
-/**
- * struct printer - A new struct type defining a printer.
- * @symbol: A symbol representing a data type.
- * @print: A function pointer to a function that prints
- *         a data type corresponding to symbol.
- */
-
-typedef struct printer
-{
-	char *symbol;
-	void (*print)(va_list arg);
-
-} printer_t;
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
