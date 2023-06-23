ab0898fdcb64c606e7c5220cd1c976f526f196a7
s-channah <hannahsadofabohns@gmail.com>
2023-06-23 01:08:55 +0100

    created variadic function that prints the strings passed into it followed by \n

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints string passed to ()&separated by @separator
+ * @separator: string printed btw strings for separation
+ * @n: no of strings passed as argument to function
+ * @...: variable no of string to be printed
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char* str;
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char *);
+		if (str == NULL)
+			printf("nil");
+		else
+			printf("%s", str);
+
+		if (separator != NULL && i < (n - 1))
+		printf("%s", separator);
+	}
+
+	printf("\n");
+
+	va_end(args);
+
+}
