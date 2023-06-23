bbea6e1826f00943dd988877f346718177127a61
s-channah <hannahsadofabohns@gmail.com>
2023-06-23 02:22:39 +0100

    changes

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,9 @@
 #ifndef _VARIADIC_FUNCTIONS_H
 #define _VARIADIC_FUNCTIONS_H
 
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
+
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
