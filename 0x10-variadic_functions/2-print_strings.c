a4cce681ae132227d8b29612da8961cfc5c25a3d
s-channah <hannahsadofabohns@gmail.com>
2023-06-23 01:54:02 +0100

    added space to after the comma separating each arguments

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -29,7 +29,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 				printf("%s", str);
 
 		if (separator != NULL && i < (n - 1))
-		printf("%s", separator);
+		printf("%s ", separator);
 	}
 
 	printf("\n");
