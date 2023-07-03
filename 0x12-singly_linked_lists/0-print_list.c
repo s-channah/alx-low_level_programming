9db2e157c9e60de6960190d20cab8332e6782f44
s-channah <hannahsadofabohns@gmail.com>
2023-07-04 00:47:56 +0100

    list all elements in node

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - ...
+ * @h: ...
+ * Return: ...
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Singly Linked List begins Today
