93e82bf2e7a73fd7afa35682c9a832c66d5e5223
s-channah <hannahsadofabohns@gmail.com>
2023-07-11 05:20:41 +0100

    created README.md, function that reads text and prints it to POSIX stdout

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * read_textfile - ...
+ * @filename: ...
+ * @letters: ...
+ * Return:...
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t nread, nwritten;
+	int fd;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * (letters + 1));
+
+	if (buffer == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	nread = read(fd, buffer, letters);
+	nwritten = write(STDOUT_FILENO, buffer, nread);
+
+	if (fd == -1 || nread == -1 || nwritten == -1 || nwritten != nread)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	return (nwritten);
+}
