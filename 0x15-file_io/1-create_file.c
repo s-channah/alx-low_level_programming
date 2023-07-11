de2517b952116f4717d61e8ccbd8fc141457c6cf
s-channah <hannahsadofabohns@gmail.com>
2023-07-11 05:31:39 +0100

    created function that creates a file and writes contents into it

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_file - ...
+ * @filename: ...
+ * @text_content: ...
+ * Return: ...
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, nwritten, len;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len]; len++)
+			;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	nwritten = write(fd, text_content, len);
+
+	if (fd == -1 || nwritten != len || nwritten == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
